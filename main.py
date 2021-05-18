e0ecbdeefd2a6401ede63881f340e86e420961f4
Océane MACHADO <oceane.machado@ens.uvsq.fr>
2021-05-18 14:50:33 +0200

    correction et bouton reset

--- main.py
+++ main.py
@@ -10,10 +10,16 @@
 # https://github.com/uvsq21915611/projet_robot
 ###################################################
 
+
+#import des librairies
+
+import sys
+import os
 import tkinter as tk
+from tkinter.constants import END, LEFT
 racine = tk.Tk()
-racine.title("Projet robot ricochet")
-racine.configure(width = 300, height = 300, bg ='beige')
+racine.title("Projet Robot Ricochet")
+racine.configure(width = 500, height = 500, bg ='beige')
 
 #variable
 c = 40                         # Longueur d'un côté d'une case
@@ -23,87 +29,30 @@ WIDTH = n*c+2
 HEIGHT = WIDTH
 stop = False
 # position initiale des robots
-PosX1 = 142
-PosY1 = 463
-PosX2 = 342
-PosY2 = 382
-PosX3 = 382
-PosY3 = 262
-PosX4 = 61
-PosY4 = 502
-
-def grille():
-    for ligne in range(n):
-        transit = []
-        for colonne in range(n):
-            transit.append(canvas.create_rectangle(colonne*c+2, ligne*c+2, (colonne+1)*c+2, (ligne+1)*c+2))
-        cases.append(transit)
-
-#Crée les robot et leurs paramètres
+X1 = 142
+Y1 = 463
+X2 = 342
+Y2 = 382
+X3 = 382
+Y3 = 262
+X4 = 61
+Y4 = 502
 
 
-#Définit quel robot de couleur est actuellement sélectionné
-def selectionne():
-    pass
-
-#Associe les touches directionnelles du clavier "haut","bas","gauche","droite" au déplacement du robot
-def Clavier(event):
-    """ Gestion de l'événement Appui sur une touche du clavier """
-    global PosX1, PosY1, PosX2, PosY2, PosX3, PosY3, PosX4, PosY4
-    touche = event.keysym
-    print(touche)
-    # déplacement vers le haut
-    if touche == 'Up':
-        PosY1 -= 40
-        PosY2 -= 40 
-        PosY3 -= 40
-        PosY4 -= 40
-    # déplacement vers le bas
-    if touche == 'Down':
-        PosY1 += 40
-        PosY2 += 40
-        PosY3 += 40
-        PosY4 += 40
-    # déplacement vers la droite
-    if touche == 'Right':
-        PosX1 += 40
-        PosX2 += 40
-        PosX3 += 40
-        PosX4 += 40
-    # déplacement vers la gauche
-    if touche == 'Left':
-        PosX1 -= 40
-        PosX2 -= 40
-        PosX3 -= 40
-        PosX4 -= 40
-    # on dessine le pion à sa nouvelle position
-    canvas.coords(PionV,PosX1 -15, PosY1 -15, PosX1 +15, PosY1 +15)
-    canvas.coords(PionR,PosX2 -15, PosY2 -15, PosX2 +15, PosY2 +15)
-    canvas.coords(PionB,PosX3 -15, PosY3 -15, PosX3 +15, PosY3 +15)
-    canvas.coords(PionJ,PosX4 -15, PosY4 -15, PosX4 +15, PosY4 +15)
-
-#Permet de faire arrêter le robot s'il rencontre un obstacle / distance maximale de déplacement autorisé
-def bounce():
-    pass
-
-#Affiche un rectangle de couleur permettant de montrer les directions possibles par le robot 
-def show_Path():
-    pass
-
-#Fait recommencer le jeu
-def est_dans_carre(x, y):
-    """Retourne True si le point de coordonnées (x, y)
-    est dans le rectangle et False sinon"""
-    return 350 + 2 < x < 450 + 2 and 350 + 2 < y < 450 + 2
+#Fonction
 
 
-def reset(event):
-    """Recommence la partie si clic sur le carre"""
-    global stop
-    if est_dans_carre(event.x, event.y) and not stop:
-        pass
-    else:
-        stop = True
+
+
+def reset():
+    """ Redémarre le programme """
+    python = sys.executable
+    os.execl(python,python, * sys.argv)
+    
+
+
+def color(fill):
+    fill = ''
 
 #Affiche le score / liste des meilleurs scores
 def score():
@@ -129,20 +78,67 @@ def editor():
 def move(event):
     pass
 
+#Permet de faire arrêter le robot s'il rencontre un obstacle / distance maximale de déplacement autorisé
+def bounce():
+    pass
+
+#Affiche un rectangle de couleur permettant de montrer les directions possibles par le robot 
+def show_Path():
+    pass
+
+#Définit quel robot de couleur est actuellement sélectionné
+def selectionne(event):
+    """ Permet de faire la selection d'un pion pour le bouger """
+    pass
 
 
+#Permet de faire arrêter le robot s'il rencontre un obstacle / distance maximale de déplacement autorisé
+
+#Associe les touches directionnelles du clavier "haut","bas","gauche","droite" au déplacement du robot
+def Clavier(event):
+    global X1, X2, X3, X4, Y1, Y2, Y3, Y4
+    touche = event.keysym
+    print(touche)
+    # déplacement vers le haut
+    if touche == 'Up':
+        Y1 -= 200
+        Y2 -= 200 
+        Y3 -= 200
+        Y4 -= 200
+    # déplacement vers le bas
+    if touche == 'Down':
+        Y2 += 200
+        Y3 += 200
+        Y4 += 200
+        Y1 += 200
+    # déplacement vers la droite
+    if touche == 'Right':
+        X2 += 200
+        X3 += 200
+        X4 += 200
+        X1 += 200
+    # déplacement vers la gauche
+    if touche == 'Left':
+        X2 -= 200
+        X3 -= 200
+        X4 -= 200
+        X1 -= 200
+    # on dessine le pion à sa nouvelle position
+    canvas.coords(PionV, X1-15, Y1-15, X1+15, Y1+15)
+    canvas.coords(PionR, X2-15, Y2-15, X2+15, Y2+15)
+    canvas.coords(PionB, X3-15, Y3-15, X3+15, Y3+15)
+    canvas.coords(PionJ, X4-15, Y4-15, X4+15, Y4+15)
+
+
+# Création d'un widget Canvas (zone graphique)
 canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg="white")
-PionV = canvas.create_oval(PosX1-15, PosY1-15, PosX1+15, PosY1+15, width=2, fill='green')
-PionR = canvas.create_oval(PosX2-15, PosY2-15, PosX2+15, PosY2+15, width=2, fill='red')
-PionB = canvas.create_oval(PosX3-15, PosY3-15, PosX3+15, PosY3+15, width=2, fill='blue')
-PionJ = canvas.create_oval(PosX4-15, PosY4-15, PosX4+15, PosY4+15, width=2, fill='yellow')
+PionV = canvas.create_oval(X1-15, Y1-15, X1+15, Y1+15, width=2, fill='green')
+PionR = canvas.create_oval(X2-15, Y2-15, X2+15, Y2+15, width=2, fill='red')
+PionB = canvas.create_oval(X3-15, Y3-15, X3+15, Y3+15, width=2, fill='blue')
+PionJ = canvas.create_oval(X4-15, Y4-15, X4+15, Y4+15, width=2, fill='yellow')
 canvas.focus_set()
 canvas.bind('<Key>',Clavier)
 canvas.grid(row =1, column =0)
-
-canvas_move = tk.Canvas(racine, width= WIDTH/5, height= HEIGHT, bg="black")
-canvas_move.bind('Key', move)
-canvas_move.grid(row=1, column=1)
 
 #création grille
 for ligne in range(n):
@@ -196,19 +192,33 @@ mur_horizontal20 = canvas.create_rectangle(440+2, 600-2, 480+2, 600+5, fill="bla
 carre_restart = canvas.create_rectangle(280+2, 280+2, 360+2, 360+2, fill="black")
 
 
+
+canvas_move = tk.Canvas(racine, width= WIDTH/5, height= HEIGHT, bg="black")
+canvas_move.bind('Key', move)
+canvas_move.grid(row=1, column=1)
+
+
+
+
+
+#Bouton label etc...
+# Création d'un widget Button (bouton Quitter)
+quitter = tk.Button(racine, text ='Quitter', command = racine.destroy)
+quitter.grid(row=6, column=0)
+# Création d'un label Button (manière de jouer)
 explication = tk.Label(racine, text ='Selectionne un pion avec ta souris puis utilise les fléches de ton clavier pour déplacer.')
 explication.grid(row=0, column=0)
 
-sauvegarde = tk.Button(racine, text="sauvegarder", command=save())
+sauvegarde = tk.Button(racine, text="Sauvegarder", command=save())
 sauvegarde.grid(row=6, column= 1)
 
-texte_compteur = tk.Label(racine, text=("Nombre de déplacements:  ",a))
+texte_compteur = tk.Label(racine, text="Nombre de déplacements:  ")
 texte_compteur.grid(row=0, column=3)
 
 texte_resultat = tk.Label(racine, text="Jeu résolu: NON", bg="red", fg="white")
 texte_resultat.grid(row=0, column=2)
 
-quitter = tk.Button(racine, text ='Quitter', command = racine.destroy)
-quitter.grid(row=6, column=0)
+recommencer = tk.Button(racine, text="Reset", command=reset)
+recommencer.grid(row=1, column= 0)
 
 racine.mainloop()
